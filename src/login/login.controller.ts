27a964d393c7a12200a7691b92cfe78209062f8c
Bíró Kálmán <linkee12@gmail.com>
2024-08-02 11:49:20 +0200

    nest try

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,14 +1,16 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { userBodyDto } from './userBodyDto';
 import { LoginService } from './login.service';
+import { users } from '@prisma/client';
 
 @Controller('login')
 export class LoginController {
   constructor(private loginService: LoginService) {}
   @Post()
   @HttpCode(200)
-  async create(@Body() userBody: userBodyDto) {
+  async create(@Body() userBody: userBodyDto): Promise<users | null> {
     const user = await this.loginService.getUserByEmail(userBody.email);
+    console.log(user?.id);
     return user;
   }
 }
